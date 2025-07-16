cf6f745500ba948b3abf1b52c562d2ede7b2ab44
Dawud <dawud@iastate.edu>
2025-07-16 16:05:39 -0500

    Added ball support

--- Field_Output_test.py
+++ Field_Output_test.py
@@ -4,7 +4,7 @@ def print_space(count):
   for i in range(count):
       print(" ", end='')
 
-def print_field(row, col):
+def print_field(row, col, p1_owner):
   print("    +–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+")
   
 
@@ -20,25 +20,33 @@ def print_field(row, col):
     # first line
     print("    |   ", end='')
     print_space(spaces)
-    print(" O ", end='')
-    print_space(58 - spaces)
+    print(" O      O ", end='')
+    print_space(50 - spaces)
     print("|")
     
     # second line
     print("    |   ", end='')
     print_space(spaces)
-    print("/|\\", end='')
-    print_space(58 - spaces)
+    print("/|\\    /|\\", end='')
+    print_space(50 - spaces)
     print("|") 
 
     # third line
     print("+---|   ", end='')
     print_space(spaces)
     print("/ \\", end='')
-    print_space(58 - spaces)
+    if p1_owner:
+      print("*   / \\", end='')
+    else:
+      print("   */ \\", end='')
+    print_space(50 - spaces)
     print("|---+")
 
 
 for i in range(-3, 3):
-  print_field(1, i)
+  print_field(1, i, True)
+  print()
+
+for i in range(-3, 3):
+  print_field(1, i, False)
   print()
